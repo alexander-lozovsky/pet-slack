ffa32dfb4fdb67acf465f06fcb53596286a32492
Fitz354 <lozovsky.alx@yandex.ru>
2018-02-15 20:04:33 +0300

    change li index

--- app/components/ChannelsList.jsx
+++ app/components/ChannelsList.jsx
@@ -8,13 +8,13 @@ export default class ChannelsList extends React.Component {
     return (
       <div className='channels-list'>
         <ul className='list-group'>
-          {channels.map(({ id, name }, index) => {
+          {channels.map(({ id, name }) => {
             const channelClass = cn({
               'list-group-item': true,
               active: id === currentChannelId,
             });
 
-            return <li className={channelClass} key={index}>{name}</li>;
+            return <li className={channelClass} key={id}>{name}</li>;
           })}
         </ul>
       </div>
